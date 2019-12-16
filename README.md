e7d831e0655810b3adc1a2f76300c17589c3d09d
arthurarty <arthur.nangai@gmail.com>
2019-12-16 22:43:20 +0300

    ch(db) - Setup postgres db.
    - Install psycopg2.
    - Update django settings to use postgres.
    - Setup env variables.
    - Update README.md file.
    
    [Finishes #170297910]

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # momo_bank_api
+
+#### Requirements to run application.
+- Python > 3.5
+- Postgres
+- Postman
+
+#### How to set up appliction.
+- Clone repository.
+- Create virtual venv. (You could use the command `python3 -m venv project_env`)
+- Create postgres database.
+- Inside the `momo_bank_api` root folder create .env file from .env_example.
+- Run migrations using the command. `python manage.py migrate`.
+- Run application using the command. `python manage.py runserver`.
--- momo_bank_api/settings.py
+++ momo_bank_api/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,12 +22,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wp6hg83&vatfe%ldmiy#5mnjj=r65p^($rzadq)nne7i3#ryz0'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv('BACKEND_HOST'),
+]
 
 
 # Application definition
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'momo_bank_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'landreg'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'PORT': '5432'
     }
 }
 
