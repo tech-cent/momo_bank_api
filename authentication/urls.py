1a0ea5778b2b216e66700b5d0a496e1f7f9c9cc2
arthurarty <arthur.nangai@gmail.com>
2019-12-21 23:05:19 +0300

    ft(login) - Add login endpoint.
    - Add simple_jwt to application.
    - Add settings for JWT.
    - Add tests for login.
    
    [Finishes #170380162]

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
+
 from authentication import views
 
 app_name = "authentication"
 urlpatterns = [
     path('signup/', views.UserView.as_view(), name='signup'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
