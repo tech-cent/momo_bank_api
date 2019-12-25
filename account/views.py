a4c62674f285d44b5b5333ced29beb59089ddd33
arthurarty <arthur.nangai@gmail.com>
2019-12-25 13:15:14 +0300

    ft(account) - Add get account endpoint.
    - Add functionality to return acounts owned by user.
    - Add tests for functionality.
    - Refactor tests to reduce duplications.
    
    [Delivers #170425255]

--- account/views.py
+++ account/views.py
@@ -15,8 +15,19 @@ class AccountView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
+        """
+        Create a bank account for the user logged in.
+        """
         serializer = AccountSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        """
+        Return all accounts belonging to the user logged in.
+        """
+        accounts = request.user.account_set.all()
+        serializer = AccountSerializer(accounts, many=True)
+        return Response(serializer.data)
