c05edf2b11e41251fffbca5d00eaeaf7b3ad81f5
arthurarty <arthur.nangai@gmail.com>
2019-12-30 22:29:46 +0300

    bg(signup) - Edit error message.
    -  Ensure error message includes NIN since NIN must be unique.
    - Add tests for functionality.
    
    [Delivers #170441402]

--- tests/sample_data/authentication.py
+++ tests/sample_data/authentication.py
@@ -6,6 +6,24 @@ user_1 = {
     "dob": "1995-04-22",
     "nin": "userNinNumber"
 }
+
+user_duplicate_nin = {
+    "phone_number": "25670000002",
+    "password": "thisIs6@5",
+    "first_name": "John",
+    "last_name": "Doe",
+    "dob": "1995-04-22",
+    "nin": "userNinNumber"
+}
+
+incomplete_user = {
+    "phone_number": "25670000002",
+    "password": "thisIs6@5",
+    "first_name": "John",
+    "dob": "1995-04-22",
+    "nin": "userNinNumber"
+}
+
 user_1_login = {
     "phone_number": "25670000000",
     "password": "thisIs6@5",
