392aadec25482790c61bda0f207b4464e790da5b
Arthur Nangai <arthurarty@users.noreply.github.com>
2019-12-30 22:39:14 +0300

    bg(signup) - Edit error message. (#16)
    -  Ensure error message includes NIN since NIN must be unique.
    - Add tests for functionality.
    
    [Delivers #170441402]

--- authentication/views.py
+++ authentication/views.py
@@ -18,7 +18,7 @@ class UserView(APIView):
                 serializer.save()
             except IntegrityError:
                 return Response(
-                    "Number already exists",
+                    "Number or NIN already exists",
                     status=status.HTTP_400_BAD_REQUEST)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,7 +3,8 @@ from rest_framework import status
 
 from authentication.models import User
 from tests.base import BaseTestCase
-from tests.sample_data.authentication import user_1, user_1_login
+from tests.sample_data.authentication import (incomplete_user, user_1,
+                                              user_1_login, user_duplicate_nin)
 
 
 class AuthTests(BaseTestCase):
@@ -20,6 +21,24 @@ class AuthTests(BaseTestCase):
         """
         self.signup_user(user_1)
         response = self.signup_user(user_1)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, 'Number or NIN already exists')
+
+    def test_duplicate_nin(self):
+        """
+        Ensure error message for already existing
+        NIN
+        """
+        self.signup_user(user_1)
+        response = self.signup_user(user_duplicate_nin)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, 'Number or NIN already exists')
+
+    def test_incomplete_user(self):
+        """
+        Ensure 400 error if a field is missing
+        """
+        response = self.signup_user(incomplete_user)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_login(self):
