9dfbcf45afdea97fed44cc4f0760f316cf8a1f2a
arthurarty <arthur.nangai@gmail.com>
2020-01-06 19:04:35 +0300

    ft(tooken) - Add user name.

--- authentication/views.py
+++ authentication/views.py
@@ -2,9 +2,11 @@ from django.db import IntegrityError
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from authentication.models import User
-from authentication.serializers import UserSerializer
+from authentication.serializers import (CustomTokenObtainPairSerializer,
+                                        UserSerializer)
 
 
 class UserView(APIView):
@@ -22,3 +24,7 @@ class UserView(APIView):
                     status=status.HTTP_400_BAD_REQUEST)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
