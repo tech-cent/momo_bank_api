872922307fd6289480ab0d3a44a51924c4cc8af8
Arthur Nangai <arthurarty@users.noreply.github.com>
2020-01-11 15:19:56 +0300

    ft(docs) - Implement swagger documentation. (#22)
    - Update readme to include endpoint descriptions.
    - Install and configure Swagger documentation.
    
    [Delievers #170441423]

--- momo_bank_api/settings.py
+++ momo_bank_api/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'drf_yasg',
     'transactions.apps.TransactionsConfig',
     'account.apps.AccountConfig',
     'bank.apps.BankConfig',
