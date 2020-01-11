e8632d05856e24a9a9ffddd31ae5531cfafb518d
Arthur Nangai <arthurarty@users.noreply.github.com>
2020-01-11 15:40:51 +0300

    ch(static) - Add static to urls. (#23)

--- momo_bank_api/urls.py
+++ momo_bank_api/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
@@ -43,4 +45,4 @@ urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
