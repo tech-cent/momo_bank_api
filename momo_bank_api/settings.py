c0f32b692cd37ee171858f976ce4444d8f46928a
Arthur Nangai <arthurarty@users.noreply.github.com>
2020-01-23 22:25:07 +0300

    ch(tokens) - Extend token expiry to 6 hours. (#29)

--- momo_bank_api/settings.py
+++ momo_bank_api/settings.py
@@ -146,7 +146,7 @@ AUTH_USER_MODEL = 'authentication.User'
 
 # Settings for simple jwt token
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=360),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
